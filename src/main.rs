ce0592e15523498bc454ab399bf1e7c1785c69fe
Caleb Chiang <calebchiang1002@gmail.com>
2024-06-03 20:19:20 -0700

    Updated to use trait bounds.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-fn largest(list: &[i32]) -> i32 {
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
     let mut largest = list[0];
     for &item in list.iter(){
         if item > largest {
@@ -8,12 +8,12 @@ fn largest(list: &[i32]) -> i32 {
 }
 
 fn main() {
-    let number_list = vec![34, 50, 25,100, 65];
+    let number_list = vec![34, 50, 25, 100, 65];
     let result = largest(&number_list);
     println!("The largest number is {}", result);
 
 
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
 }
